7b5ff498639573cafa53a2fca9134c124c9dc5bf
Patrick Mukherjee <patrick.mukherjee@deondigital.com>
2018-08-19 02:36:35 +0200

    refactored

--- src/main/kotlin/Xor.kt
+++ src/main/kotlin/Xor.kt
@@ -1,41 +1,54 @@
 import java.io.File
 
-const val INPUT_FOLDER = "input"
-const val OUTPUT_FOLDER = "output"
+const val INPUT_FOLDER = "."
+const val OUTPUT_FOLDER = "converted"
 
-const val EXT_MP3 = ".mp3"
-const val EXT_SMP = ".smp"
+const val EXT_MP3 = "mp3"
+const val EXT_SMP = "smp"
+
+const val USEAGE = "please provide only an existing folder or file name"
 
 const val KEY = 'f'.toInt()
 
 fun main(args: Array<String>) {
     println("StoryMania file converter")
     when {
-        args.isEmpty() -> generateSequence(1, { it + 1 })
-                .map { "M" + it.toString().padStart(2, '0') }
-                .takeWhile { toFile(INPUT_FOLDER, it, EXT_SMP).exists() }
-                .forEach { transcode(it, EXT_SMP, EXT_MP3) }
-        args[0] == "dec" -> decode(args[1])
-        args[0] == "enc" -> encode(args[1])
+        args.isEmpty() && File(INPUT_FOLDER).exists() -> println("converting files in folder '$INPUT_FOLDER'")
+        args.size > 1 -> println(USEAGE)
+        args.size == 1 -> {
+            val file = File(args[0])
+            if (!file.exists()) {
+                System.err.println("File or folder '${args[0]}' does not exist, $USEAGE")
+                System.exit(1)
+            }
+            transcode(file)
+        }
     }
 }
 
-private fun decode(filename: String) {
-    transcode(filename, EXT_SMP, EXT_MP3)
+private fun transcode(file: File){
+    if (file.isDirectory){
+        file.listFiles().map {transcode(it) }
+    }
+    if (file.isFile){
+        if (file.extension.toLowerCase() == EXT_SMP){
+            transcode(file, EXT_MP3)
+        } else if (file.extension.toLowerCase() == EXT_MP3 ){
+            transcode(file, EXT_SMP)
+        } else {
+            System.err.println("Skipped file with wrong extension '${file.canonicalFile}'")
+        }
+    }
 }
 
-private fun encode(filename: String) {
-    transcode(filename, EXT_MP3, EXT_SMP)
-}
-
-private fun transcode(filename: String, inExtension: String, outExtension: String) {
-    val input = (toFile(INPUT_FOLDER, filename, inExtension)
-            .takeIf { it.exists() } ?: return)
+private fun transcode(file: File, outExtension: String) {
+    val input = file
             .inputStream()
             .buffered()
-
-    toFile(OUTPUT_FOLDER, filename, outExtension)
-            .outputStream()
+    val output = File(OUTPUT_FOLDER, file.nameWithoutExtension+"."+outExtension)
+    output.parentFile.mkdirs()
+    output.createNewFile()
+    output.outputStream()
             .buffered()
             .use {
                 var next = input.read()
@@ -46,6 +59,3 @@ private fun transcode(filename: String, inExtension: String, outExtension: Strin
             }
 }
 
-private fun toFile(folder: String, name: String, extenstion: String): File {
-    return File(folder, name + extenstion)
-}
